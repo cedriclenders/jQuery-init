0ce3946c45ec936a4a7d395061187399f08e77dc
Cedric Lenders <cedric.lenders@student.kdg.be>
2021-05-02 19:57:26 +0200

    disabled functie verbeteren

--- test.js
+++ test.js
@@ -3,6 +3,18 @@ $(function() {
     $( "form" ).on( "submit", function( event ) {
         // stopt de default tasks van de event, namelijk form submitten
         event.preventDefault();
-        $("#outputField").append($("#ftext").val());
+        $("#outputField").append($("#ftext").val()+" ");
+        $('#ftext').val('');
+        $('#submit').prop('disabled', true);
       });
+
+      $('#ftext').on('input change', function () {
+        if ($('#ftext').val() == '') {
+            $('#submit').prop('disabled', true);
+            
+        }
+        else {
+            $('#submit').prop('disabled', false);
+        }
+    });
 })
